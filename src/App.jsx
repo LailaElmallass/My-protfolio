5f66ffa6c180c797f3cd0425be2be7d19fcc85d0
Laila <Laila.elmallass.2018@gmail.com>
2025-02-26 21:13:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// import ParticlesBackground from "./components/particles";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <Router>
+      {/* <ParticlesBackground /> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
